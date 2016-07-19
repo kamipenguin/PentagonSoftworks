5137417d3cf26f47f714059945a6a55515e96da9
Jeffrey van Zanten <jeffrey_selfmademiracle>
2016-07-19 16:47:07 +0200

    no message

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject ball;
+
+    Vector3 player1Spawn = new Vector3(0.14f, 4.44f, 4.75f);
+    Vector3 player2Spawn = new Vector3(0.14f, 4.44f, -4.75f);
+
+    // Use this for initialization
+    void Start()
+    {
+        CoinToss();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void CoinToss()
+    {
+        float tossChance = 0.5f;
+
+        if (Random.value < tossChance)
+        {
+            SpawnBall(player1Spawn);
+        }
+        else
+        {
+            SpawnBall(player2Spawn);
+        }
+    }
+
+    public void SpawnBall(Vector3 position) {
+        Instantiate(ball, position, Quaternion.identity);
+    }
+
+    public Vector3 GetPlayer1Spawn() {
+        return player1Spawn;
+    }
+
+    public Vector3 GetPlayer2Spawn() {
+        return player2Spawn;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,12 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
     [SerializeField]
-    private Transform playerLeft;
+    private Transform leftPlayer;
     [SerializeField]
-    private Transform playerRight;
+    private Transform rightPlayer;
+
+    private Rigidbody rbLeftPlayer;
+    private Rigidbody rbRightPlayer;
 
     [SerializeField]
     private float speed;
@@ -31,52 +34,56 @@ public class PlayerController : MonoBehaviour {
         print("pullright");
     }
 
-    public void moveLeft(Vector3 direction)
+    public void moveLeftPlayer(Vector3 direction)
     {
-        playerLeft.position  += direction;
+        rbLeftPlayer.position += direction * Time.deltaTime * speed;
     }
 
-    public void moveRight(Vector3 direction)
+    public void moveRightPlayer(Vector3 direction)
     {
-        playerRight.position += direction;
+        rbRightPlayer.position += direction * Time.deltaTime * speed;
     }
 
     void Start() {
-
+        rbLeftPlayer = leftPlayer.GetComponent<Rigidbody>();
+        rbRightPlayer = rightPlayer.GetComponent<Rigidbody>();
     }
 
     void Update() {
+
+        // wasd & up, left, down and right key movement
+
         // Player 1 moves forward with w or with the xbox controller by using the left analog stick
         if(Input.GetKey("w")) {
-            playerLeft.Translate(Vector3.forward * Time.deltaTime * speed);
+            leftPlayer.Translate(Vector3.forward * Time.deltaTime * speed);
         }
         // Player 1 moves left with w or with the xbox controller by using the left analog stick
         else if(Input.GetKey("a")) {
-            playerLeft.Translate(Vector3.left * Time.deltaTime * speed);
+            leftPlayer.Translate(Vector3.left * Time.deltaTime * speed);
         }
         // Player 1 moves backward with w or with the xbox controller by using the left analog stick
         else if(Input.GetKey("s")) {
-            playerLeft.Translate(Vector3.back * Time.deltaTime * speed);
+            leftPlayer.Translate(Vector3.back * Time.deltaTime * speed);
         }
         // Player 1 moves right with w or with the xbox controller by using the left analog stick
         else if(Input.GetKey("d")) {
-            playerLeft.Translate(Vector3.right * Time.deltaTime * speed);
+            leftPlayer.Translate(Vector3.right * Time.deltaTime * speed);
         }
         // Player 2 moves right with w or with the xbox controller by using the right analog stick
         if(Input.GetKey("up")) {
-            playerRight.Translate(Vector3.forward * Time.deltaTime * speed);
+            rightPlayer.Translate(Vector3.forward * Time.deltaTime * speed);
         }
         // Player 2 moves right with w or with the xbox controller by using the right analog stick
         else if(Input.GetKey("left")) {
-            playerRight.Translate(Vector3.left * Time.deltaTime * speed);
+            rightPlayer.Translate(Vector3.left * Time.deltaTime * speed);
         }
         // Player 2 moves right with w or with the xbox controller by using the right analog stick
         else if(Input.GetKey("down")) {
-            playerRight.Translate(Vector3.back * Time.deltaTime * speed);
+            rightPlayer.Translate(Vector3.back * Time.deltaTime * speed);
         }
         // Player 2 moves right with w or with the xbox controller by using the right analog stick
         else if(Input.GetKey("right")) {
-            playerRight.Translate(Vector3.right * Time.deltaTime * speed);
+            rightPlayer.Translate(Vector3.right * Time.deltaTime * speed);
         }
     }
 }
