3c72dccaf8758fe485a9f47b8c73caa15d96be23
timgoeij <tim_goeijenbier@live.nl>
2016-07-19 12:26:43 +0200

    movement with xbox controllers work

---
+++ Assets/Scripts/JoystickController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JoystickController : MonoBehaviour
+{
+    private PlayerController player1;
+    private PlayerController player2;
+    
+    
+    // Use this for initialization
+    void Start()
+    {
+        player1 = GameObject.FindWithTag("player1").GetComponent<PlayerController>();
+        //player2 = GameObject.FindWithTag("player2").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Joystick1Button4))
+            player1.pushLeft();
+
+        if (Input.GetKey(KeyCode.Joystick1Button5))
+            player1.pushRight();
+
+        if (Input.GetAxis("Player1PullLeft") == 1)
+            player1.pullLeft();
+
+
+        if (Input.GetAxis("Player1PullRight") == 1)
+            player1.pullRight();
+
+        if (Input.GetAxis("HorizontalPlayer1Left") != 0 || Input.GetAxis("VerticalPlayer1Left") != 0)
+        {
+            Vector3 direction = new Vector3(Input.GetAxis("HorizontalPlayer1Left"), 0, 
+                -Input.GetAxis("VerticalPlayer1Left")) * Time.deltaTime;
+
+            player1.moveLeft(direction);
+        }
+
+        if (Input.GetAxis("HorizontalPlayer1Right") != 0 || Input.GetAxis("VerticalPlayer1Right") != 0)
+        {
+            Vector3 direction = new Vector3(Input.GetAxis("HorizontalPlayer1Right"), 0,
+                -Input.GetAxis("VerticalPlayer1Right")) * Time.deltaTime;
+
+            player1.moveRight(direction);
+        }
+
+        //print(Input.GetAxis("player1FireLeft"));
+        //print(Input.GetAxis("player1FireRight"));
+        /*print(Input.GetAxis("player2FireLeft"));
+        print(Input.GetAxis("player2FireRight"));*/
+
+    }
+}
