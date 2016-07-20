9ab81a06695f7b4f16da3da3a625794643d36f65
timgoeij <tim_goeijenbier@live.nl>
2016-07-20 12:29:00 +0200

    movement update and adding character

--- Assets/Scripts/JoystickController.cs
+++ Assets/Scripts/JoystickController.cs
@@ -11,35 +11,82 @@ public class JoystickController : MonoBehaviour
     void Start()
     {
         player1 = GameObject.FindWithTag("player1").GetComponent<PlayerController>();
-        //player2 = GameObject.FindWithTag("player2").GetComponent<PlayerController>();
+        player2 = GameObject.FindWithTag("player2").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Joystick1Button4))
+        if (Input.GetKey("joystick 1 button 4"))
+        {
             player1.pushLeft();
+        }
 
-        if (Input.GetKey(KeyCode.Joystick1Button5))
+        if (Input.GetKey("joystick 2 button 4"))
+        {
+            player2.pushLeft();
+        }
+
+        if (Input.GetKey("joystick 1 button 5"))
+        {
             player1.pushRight();
+        }
 
-        if (Input.GetAxis("Player1PullLeft") == 1)
+        if (Input.GetKey("joystick 2 button 5"))
+        {
+            player2.pushRight();
+        }
+
+        /*if (Input.GetAxis("Player1PullLeft") == 1)
             player1.pullLeft();
 
 
         if (Input.GetAxis("Player1PullRight") == 1)
-            player1.pullRight();
+            player1.pullRight();*/
 
-        if (Input.GetAxis("HorizontalPlayer1Left") != 0 || Input.GetAxis("VerticalPlayer1Left") != 0)
-            print("move");
 
-        if (Input.GetAxis("HorizontalPlayer1Right") != 0 || Input.GetAxis("VerticalPlayer1Right") != 0)
-            print("move right");
 
-        //print(Input.GetAxis("player1FireLeft"));
-        //print(Input.GetAxis("player1FireRight"));
-        /*print(Input.GetAxis("player2FireLeft"));
-        print(Input.GetAxis("player2FireRight"));*/
+        if (Mathf.Round(Input.GetAxis("HorizontalPlayer1Left")) != 0 || Mathf.Round(Input.GetAxis("VerticalPlayer1Right")) != 0)
+        {
+            Vector3 direction = new Vector3(Input.GetAxis("HorizontalPlayer1Left"), 0,
+                -Input.GetAxis("VerticalPlayer1Left")) * Time.deltaTime;
+
+            player1.moveLeft(direction);
+        }
+        else
+            player1.moveLeft(Vector3.zero);
+            
+
+        if (Mathf.Round(Input.GetAxis("HorizontalPlayer1Right")) != 0 || Mathf.Round(Input.GetAxis("VerticalPlayer1Right")) != 0)
+        {
+            Vector3 direction = new Vector3(Input.GetAxis("HorizontalPlayer1Right"), 0,
+                -Input.GetAxis("VerticalPlayer1Right")) * Time.deltaTime;
+
+            player1.moveRight(direction);
+        }
+        else
+            player1.moveRight(Vector3.zero);
+
+        if (Mathf.Round(Input.GetAxis("HorizontalPlayer2Left")) != 0 || Mathf.Round(Input.GetAxis("VerticalPlayer2Left")) != 0)
+        {
+            Vector3 direction = new Vector3(Input.GetAxis("HorizontalPlayer2Left"), 0,
+                -Input.GetAxis("VerticalPlayer2Left")) * Time.deltaTime;
+
+            player2.moveLeft(direction);
+        }
+        else
+            player2.moveLeft(Vector3.zero);
+
+
+        if (Mathf.Round(Input.GetAxis("HorizontalPlayer2Right")) != 0 || Mathf.Round(Input.GetAxis("VerticalPlayer2Right")) != 0)
+        {
+            Vector3 direction = new Vector3(Input.GetAxis("HorizontalPlayer2Right"), 0,
+                -Input.GetAxis("VerticalPlayer2Right")) * Time.deltaTime;
+
+            player2.moveRight(direction);
+        }
+        else
+            player2.moveRight(Vector3.zero);
 
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,38 +3,110 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-    public Transform playerLeft;
-    public Transform playerRight;
+    [SerializeField]
+    private Transform playerLeft;
+    [SerializeField]
+    private Transform playerRight;
 
-    public Transform ball;
+    [SerializeField]
+    private float speed;
+
+    private Rigidbody ballBody;
+    private Ball ball;
+
+    private float distanceToBall = 0;
+
+    private Rigidbody body;
+
+    void Start()
+    {
+        ballBody = GameObject.FindWithTag("Ball").transform.GetComponent<Rigidbody>();
+        ball = GameObject.FindWithTag("Ball").transform.GetComponent<Ball>();
+        body = playerLeft.GetComponent<Rigidbody>();
+    }
     
     public void pushLeft()
     {
-        print("pushleft");
+        distanceToBall = Vector3.Distance(ballBody.position, body.position);
+
+        if (distanceToBall < 10)
+            ball.push(playerLeft);
+
+        //print(distanceToBall.magnitude);
+        print(Vector3.Distance(ballBody.position, body.position));
     }
 
     public void pushRight()
     {
         print("pushright");
+        ball.pull = false;
     }
 
-    public void pullLeft()
+    /*public void pullLeft()
     {
-        print("pullleft");
+        /*distanceToBall = ballBody.position + body.position;
+
+        if (distanceToBall.magnitude <= 7)
+        {
+            
+            
+        /*}
+        else
+            ball.pull = false;
+
+        ball.pull = true;
+        ball.pulling(playerLeft.position);
     }
 
     public void pullRight()
     {
         print("pullright");
-    }
+    }*/
 
     public void moveLeft(Vector3 direction)
     {
-        playerLeft.position  += direction;
+        //body = playerLeft.GetComponent<Rigidbody>();
+        body.position += direction * speed;
     }
 
     public void moveRight(Vector3 direction)
     {
-        playerRight.position += direction;
+        Rigidbody body = playerRight.GetComponent<Rigidbody>();
+        body.position += direction * speed;
+    }
+
+    void Update() {
+        // Player 1 moves forward with w or with the xbox controller by using the left analog stick
+        if(Input.GetKey("w")) {
+            playerLeft.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+        // Player 1 moves left with w or with the xbox controller by using the left analog stick
+        else if(Input.GetKey("a")) {
+            playerLeft.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        // Player 1 moves backward with w or with the xbox controller by using the left analog stick
+        else if(Input.GetKey("s")) {
+            playerLeft.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+        // Player 1 moves right with w or with the xbox controller by using the left analog stick
+        else if(Input.GetKey("d")) {
+            playerLeft.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+        // Player 2 moves right with w or with the xbox controller by using the right analog stick
+        if(Input.GetKey("up")) {
+            playerRight.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+        // Player 2 moves right with w or with the xbox controller by using the right analog stick
+        else if(Input.GetKey("left")) {
+            playerRight.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+        // Player 2 moves right with w or with the xbox controller by using the right analog stick
+        else if(Input.GetKey("down")) {
+            playerRight.Translate(Vector3.back * Time.deltaTime * speed);
+        }
+        // Player 2 moves right with w or with the xbox controller by using the right analog stick
+        else if(Input.GetKey("right")) {
+            playerRight.Translate(Vector3.right * Time.deltaTime * speed);
+        }
     }
 }
