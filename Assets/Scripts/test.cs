8a22a24bba5b2604eca453da547204ae64a31ead
timgoeij <tim_goeijenbier@live.nl>
2016-07-21 20:54:18 +0200

    trail add

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.position += Vector3.forward * Time.deltaTime * 10;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.position += Vector3.back * Time.deltaTime * 10;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.position += Vector3.left * Time.deltaTime * 10;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.position += Vector3.right * Time.deltaTime * 10;
+        }
+
+    }
+}
